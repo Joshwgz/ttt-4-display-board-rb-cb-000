db33382c76b13e487d656244f96e760c0f61716d
Joshwgz <jbillguzman@gmail.com>
2017-11-07 04:56:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,7 +5,9 @@ def display_board(board = ["   ", "|", "   ", "|", "   ",
   "   ", "|", "   ", "|", "   ",
   "-----------",
   "   ", "|", "   ", "|", "   "])
+  puts board
+  
 end
 
 
-  display_board(board)
+  display_boar
